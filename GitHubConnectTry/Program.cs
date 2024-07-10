93f5cf79e5583ff1712391e39881f074c0f474ba
Hayk Grigoryan <grig.aik@gmail.com>
2024-07-10 17:46:56 +0400

    Added Clearing and Readkey to Run.Logic Branch

--- GitHubConnectTry/Program.cs
+++ GitHubConnectTry/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 Console.WriteLine("Hello, Academy! Tryin to connect GitHub");
 Console.WriteLine("This line was added directly in GitHub");
+Console.WriteLine("App is running...Please wait");
+Thread.Sleep(1000);
+Console.Clear();
+Console.ReadKey();
