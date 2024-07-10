7fcbb99b3c1b01163d8fdbefde71bfefe3c9adfa
Hayk Grigoryan <grig.aik@gmail.com>
2024-07-10 17:20:44 +0400

    Revert "Added 3rd line to the program"
    This reverts commit 60a4228eb5c97bf81b8fd2021cb43c38e2e658ca.

--- GitHubConnectTry/Program.cs
+++ GitHubConnectTry/Program.cs
@@ -1,4 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Academy! Tryin to connect GitHub");
-Console.WriteLine("This line was added directly in GitHub");
-Console.WriteLine("This line is for testing Fetch option");
+Console.WriteLine("This line was added directly in GitHub")
