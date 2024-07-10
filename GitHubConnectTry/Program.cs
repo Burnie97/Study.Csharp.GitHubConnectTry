e44079c9a2d107bf4b98b913d7b0baf0697f668c
Burnie97 <grig.aik@gmail.com>
2024-07-10 17:10:48 +0400

    added one more message

--- GitHubConnectTry/Program.cs
+++ GitHubConnectTry/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Academy! Tryin to connect GitHub");
+Console.WriteLine("This line was added directly in GitHub")
